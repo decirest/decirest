355fe9063854c1943add7d6d6866fe4f4183b64a
alexei-salikhov <alexei.salikhov@sigicom.com>
2020-12-14 11:40:04 +0100

    added wrappers for cowboy types

--- src/decirest_req.erl
+++ src/decirest_req.erl
@@ -15,11 +15,17 @@
   reply/4
 ]).
 
+%% Types
+-type http_status() :: cowboy:http_status().
+-type req() :: cowboy_req:req().
+-type http_headers() :: cowboy:http_headers().
+-type resp_body() :: cowboy_req:resp_body().
 
--spec reply(non_neg_integer(), map()) -> map() | error.
+
+-spec reply(http_status(), Req) -> Req when Req::req().
 reply(Status, Req) ->
   cowboy_req:reply(Status, Req).
 
--spec reply(non_neg_integer(), map(), map(), map()) -> map() | error.
+-spec reply(http_status(), http_headers(), resp_body(), Req) -> Req when Req::req().
 reply(Status, Header, Body, Req) ->
   cowboy_req:reply(Status, Header, Body, Req).
