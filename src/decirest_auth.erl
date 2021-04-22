dfa5c9622e79bd6aa5545cc915280c7078fd38ba
sharyari <sharyari@gmail.com>
2021-04-22 08:46:48 +0200

    mro fix for gate2

--- src/decirest_auth.erl
+++ src/decirest_auth.erl
@@ -46,9 +46,18 @@ gate1(Req, State = #{module := Module, decirest_auth_module := AuthModule}) ->
 gate1(Req, State) ->
   {true, Req, State}.
 
+
 -spec gate2(_,_) -> any().
 gate2(Req, State = #{module := Module, decirest_auth_module := AuthModule}) ->
-  decirest:apply_with_default(Module, gate2, [Req, State], fun AuthModule:gate2/2);
+  case maps:get(main_module, State) of
+    Module ->
+      decirest:apply_with_default(Module, gate2, [Req, State], fun AuthModule:gate2/2);
+    _ ->
+      Method = decirest_req:method(Req),
+      MroReq = decirest_req:set_method(Req, <<"GET">>),
+      {B, NewReq, NewState} = decirest:apply_with_default(Module, gate2, [MroReq, State], fun AuthModule:gate2/2),
+      {B, decirest_req:set_method(NewReq, Method), NewState}
+  end;
 gate2(Req, State) ->
   {true, Req, State}.
 
